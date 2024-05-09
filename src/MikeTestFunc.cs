849f23c3cc15b356f293902d2f097579df507a6c
Mike Hollis <hollis21@hotmail.com>
2024-05-08 20:13:53 -0400

    Initial Commit

---
+++ src/MikeTestFunc.cs
@@ -0,0 +1,24 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocalFunctionProj
+{
+    public class MikeTestFunc
+    {
+        private readonly ILogger<MikeTestFunc> _logger;
+
+        public MikeTestFunc(ILogger<MikeTestFunc> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("MikeTestFunc")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions 2!");
+        }
+    }
+}
